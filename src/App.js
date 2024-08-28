869eb222bdf89c05865216a59f3a8698fe7a2c4f
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-08-28 12:26:38 -0300

    feat: index and app modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 
+import Header from './components/Header/Header';
+import PersonalBanner from './components/PersonalBanner/PersonalBanner';
+
 function App() {
   return (
     <div className="App">
-    
+
+        <Header nome="seu nome aqui"/>
+
+        <PersonalBanner />
+
     </div>
   );
 }
